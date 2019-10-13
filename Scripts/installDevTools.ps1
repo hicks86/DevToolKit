fc91e104dd67a1eccc95e758cbe041feee38dd2a
hicks-perfectit <rhys@intelligentediting.com>
2019-10-13 11:38:07 +0100

    Add VS2017 Workloads (need testing) and drop IF logic

--- Scripts/installDevTools.ps1
+++ Scripts/installDevTools.ps1
@@ -1,38 +1,38 @@
 Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 
-
-param([bool] $installAdditionalTools = true)
-
 choco feature enable -n allowGlobalConfirmation
 
-choco install skype 
+#Essential applications
 choco install visualstudio2017professional
+choco install visualstudio2017-workload-node
+choco install visualstudio2017-workload-netcoretools
+choco install visualstudio2017-workload-manageddesktop
+choco install visualstudio2017-workload-netweb
+choco install visualstudio2017-workload-universal
+choco install dotnet4.5.2
+choco install dotnet4.7.1
 choco install sql-server-management-studio
 choco install sql-server-express
 choco install git 
 choco install sourcetree
 choco install googlechrome 
 
+#Specific applications
+choco install appium-desktop
 
-if ($installAdditionalTools) {
-    choco install 7zip  
-    choco install notepadplusplus
-    choco install adobereader
+#Useful applications
+choco install 7zip  
+choco install notepadplusplus
+choco install adobereader
+choco install wireshark 
+choco install linqpad
+choco install vscode
 
-    choco install fiddler4 
-    choco install wireshark 
-    choco install linqpad
-    choco install appium-desktop
+choco install evernote
+choco install evernote-chrome
 
-
-    choco install docker 
-    choco install docker-for-windows
-
-    choco install vscode
-    choco install evernote
-    choco install evernote-chrome
-    choco install teamviewer
-    choco install poshgit
-}
-
-#choco install firefox
+choco install poshgit
+choco install teamviewer
+choco install rdcman
+choco install docker 
+choco install docker-for-windows
