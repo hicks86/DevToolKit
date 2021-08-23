08df666ac365a841de7ea15b4f95a70087617093
hicks-perfectit <rhys@intelligentediting.com>
2021-08-23 11:06:55 +0100

    Reorganize applications to install

--- Scripts/installDevTools.ps1
+++ Scripts/installDevTools.ps1
@@ -9,13 +9,13 @@ choco install visualstudio2019-workload-manageddesktop
 choco install visualstudio2019-workload-netcrossplat
 choco install visualstudio2019-workload-office
 choco install visualstudio2019-workload-netcoretools
-#choco install dotnet4.5.2 - ???
-#choco install dotnet4.7.1 - ???
-#choco install netfx-4.7.1-devpack - ???
 choco install git 
-choco install sourcetree
-choco install googlechrome 
-choco install brave
+#choco install sourcetree
+#choco install googlechrome 
+#choco install brave
+choco install office365business
+choco install sqlitebrowser
+choco install beyondcompare
 
 #Specific applications
 choco install appium-desktop
@@ -24,13 +24,4 @@ choco install appium-desktop
 choco install 7zip  
 choco install notepadplusplus
 choco install adobereader
-choco install wireshark 
 choco install vscode
-
-choco install evernote
-choco install evernote-chrome
-
-choco install poshgit
-choco install teamviewer
-#choco install docker 
-#choco install docker-for-windows
