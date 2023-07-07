fb60d64215e551d69deff769a6e060ba36d249ec
hicks86 <hicks86@users.noreply.github.com>
2023-07-07 16:07:12 +0100

    Update installDevTools.ps1
    Refine the list of programs to install

--- Scripts/installDevTools.ps1
+++ Scripts/installDevTools.ps1
@@ -3,25 +3,17 @@ Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-E
 choco feature enable -n allowGlobalConfirmation
 
 #Essential applications
-choco install visualstudio2019professional
-choco install visualstudio2019-workload-netweb
-choco install visualstudio2019-workload-manageddesktop
-choco install visualstudio2019-workload-netcrossplat
-choco install visualstudio2019-workload-office
-choco install visualstudio2019-workload-netcoretools
+choco install visualstudio2022professional
+choco install visualstudio2022-workload-netweb
+choco install visualstudio2022-workload-manageddesktop
+choco install visualstudio2022-workload-netcrossplat
+choco install visualstudio2022-workload-office
+choco install visualstudio2022-workload-netcoretools
 choco install git 
-#choco install sourcetree
-#choco install googlechrome 
-#choco install brave
+choco install git-fork
+choco install firefox
 choco install office365business
-choco install sqlitebrowser
-choco install beyondcompare
-
-#Specific applications
-choco install appium-desktop
 
 #Useful applications
-choco install 7zip  
 choco install notepadplusplus
-choco install adobereader
 choco install vscode
